8c889e3f7e392a4d10d8f2fb2bd5062b74d355a6
Christopher Newton <christopher.k.newton@me.com>
2016-10-17 23:16:14 +0200

    Allow passing arrays and nodelists

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-module.exports = function (el, attrs) {
-  for (let key in attrs) {
-    if ({}.hasOwnProperty.call(attrs, key)) {
-      el.setAttribute(key, attrs[key].toString().replace(/\s{2,}/g, ''))
+module.exports = function (els, attrs) {
+  var elements = []
+
+  if (els.length === undefined) {
+    elements = [els]
+  } else if (Array.isArray(els)) {
+    elements = els
+  } else if (els instanceof NodeList) {
+    for (var i = 0; i < els.length; i++) {
+      elements[i] = els[i]
+    }
+  } else {
+    throw new Error('set-attrs: els argument must be an element, array or nodelist')
+  }
+
+  if (Array.isArray(attrs)) {
+    setMatchedAttrs(elements, attrs)
+  } else if (typeof attrs === 'object') {
+    setSameAttrs(elements, attrs)
+  }
+}
+
+// Set the same attributes on one or more elements
+function setSameAttrs(elements, attrs) {
+  elements.forEach(function (el) {
+    setAttrs(el, attrs)
+  })
+}
+
+// Set matched attrs object on element by index
+function setMatchedAttrs(elements, attrs) {
+  for (var i = 0; i < elements.length; i++) {
+    if (attrs[i]) {
+      setAttrs(elements[i], attrs[i])
     }
   }
+}
+
+// Set attributes on an element
+function setAttrs(el, attrs) {
+  for (var key in attrs) {
+    if ({}.hasOwnProperty.call(attrs, key)) {
+      el.setAttribute(key, trim(attrs[key]))
+    }
+  }
+}
+
+// Trim newlines and multiple spaces from attribute
+function trim(attr) {
+  return attr.toString().replace(/(?:\r\n|\r|\n)/g, ' ').replace(/ +/g, ' ')
 }
