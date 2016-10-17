856ad16eca20c9c35a4ddf1d179ddaf2dc0a6c0f
Christopher Newton <christopher.k.newton@me.com>
2016-10-17 17:12:29 +0200

    Trim whitespace where appropriate and guard for in

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function setAttributes(el, attrs) {
-  for(var key in attrs) {
-    el.setAttribute(key, attrs[key]);
+module.exports = function (el, attrs) {
+  for (let key in attrs) {
+    if ({}.hasOwnProperty.call(attrs, key)) {
+      el.setAttribute(key, attrs[key].toString().replace(/\s{2,}/g, ''))
+    }
   }
 }
-
-module.exports = setAttributes;
